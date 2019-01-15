9f1df0601d12bbf0151b290bc6d57ed427cc8fc0
Leandro Crescencio <leandro.crescencio@datum.local>
2019-01-15 11:25:24 -0200

    projeto maven de requisições de disponibilidade

---
+++ src/main/java/healthcheck/support/ReadJSON.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2018 Elias Nogueira and Gregory Severo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package healthcheck.support;
+
+import com.github.fge.jackson.JsonLoader;
+import com.github.fge.jsonschema.core.report.ProcessingReport;
+import com.github.fge.jsonschema.main.JsonSchema;
+import com.github.fge.jsonschema.main.JsonSchemaFactory;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Read the health_check.json and assert if is valid against schema.json on resources
+ *
+ * @author Elias Nogueira, Gregory Severo
+ */
+public class ReadJSON {
+
+    //private static FileReader healthCheck;
+
+    private static String healthCheck;
+
+    /**
+     * Read the json file and transform it on an array of object
+     * @return and array of objects that contains name and url
+     * @throws Exception
+     */
+    public static List<Object[]> read() throws Exception {
+        List<Object[]> data = new ArrayList<Object[]>();
+        String fileFromMavenProperty = System.getProperty("file");
+
+        if (validaSchemaJSON()) {
+            JSONParser parser = new JSONParser();
+            //Object file = parser.parse(healthCheckFile(fileFromMavenProperty));
+            Object file = parser.parse(new FileReader(healthCheckFile(fileFromMavenProperty)));
+            JSONObject jsonObject = (JSONObject) file;
+            JSONArray tests = (JSONArray) jsonObject.get("test");
+
+            for (int i = 0; i < tests.size(); i++) {
+                String[] informations = new String[2];
+                JSONObject obj = (JSONObject) tests.get(i);
+                informations[0] = obj.get("name").toString();
+                informations[1] = obj.get("url").toString();
+                data.add(informations);
+            }
+        }
+
+        return data;
+    }
+
+    /**
+     * Assert that health_check.json in valid
+     * @return <i>true</i> if the file is valid or <i>false</i> if not
+     * @throws Exception
+     */
+    private static boolean validaSchemaJSON() throws Exception {
+        JsonSchemaFactory jsonSchemaFactory;
+        JsonSchema jsonSchema;
+        ProcessingReport processingReport;
+        boolean result;
+         
+        jsonSchemaFactory = JsonSchemaFactory.byDefault();
+        jsonSchema = jsonSchemaFactory.getJsonSchema(JsonLoader.fromResource("/schema.json"));
+        processingReport = jsonSchema.validate(JsonLoader.fromPath("health_check.json"));
+
+        result = processingReport.isSuccess();
+         
+        // an error occurs in json schema validation
+        if (!result) {
+           throw new Exception(processingReport.toString());
+        }
+         
+        return result;
+    }
+
+    /**
+     * Return the proper health_check.json. If the parameter <i>-Dfile=filename.json</i> is not set on command line
+     * the file health_check.json will be used. Otherwise the file informed will be used.
+     * @param file health_check file
+     * @return the proper health_check file
+     * @throws Exception
+     */
+    private static String healthCheckFile(String file) throws Exception {
+
+        if (file == null) {
+            healthCheck = "health_check.json";
+        } else {
+            healthCheck = file;
+        }
+
+        return healthCheck;
+    }
+
+    public static String getFileName() {
+        return healthCheck.replace(".json", "");
+    }
+
+    public static void createReportDir() {
+        new File("target/report").mkdir();
+    }
+}
---
+++ src/test/java/healthcheck/TestExecutor.java
@@ -0,0 +1,166 @@
+/*
+ * Copyright 2018 Elias Nogueira and Gregory Severo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package healthcheck;
+
+import healthcheck.support.ReadConfFile;
+import healthcheck.support.ReadJSON;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import junit.framework.TestCase;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Collection;
+import java.util.concurrent.TimeUnit;
+
+import static junit.framework.TestCase.assertTrue;
+
+/**
+ * Data Driven test that read the json file and create an execution for each
+ * pair of name and url
+ *
+ * @author Elias Nogueira, Gregory Severo
+ */
+@RunWith(Parameterized.class)
+public class TestExecutor {
+
+    @Parameterized.Parameter public String name;
+    @Parameterized.Parameter(1) public String url;
+
+    private static ExtentReports extentReports;
+    private static ExtentHtmlReporter htmlReporter;
+    private static ExtentTest testReport;
+    private boolean testStatus;
+    private String errorMessage;
+    private static long connectionTimeout = 0;
+
+    @Parameterized.Parameters(name = "{0}")
+    public static Collection<Object[]> data() throws Exception {
+        return ReadJSON.read();
+    }
+
+    @BeforeClass
+    public static void setup() {
+        ReadJSON.createReportDir();
+        htmlReporter = new ExtentHtmlReporter("target/report/" + ReadJSON.getFileName() + ".html");
+        extentReports = new ExtentReports();
+
+        extentReports.attachReporter(htmlReporter);
+
+        connectionTimeout = setConnectionTimeout();
+    }
+
+    @Test
+    public void healthCheckStatus() {
+        testReport = extentReports.createTest(name);
+
+        try {
+            assertTrue(healthCheckStatus(url));
+            testStatus = true;
+
+        } catch (Throwable e) {
+            // in the case of json validator throws an exception
+            errorMessage = e.getMessage().toString();
+            TestCase.fail(errorMessage);
+            testStatus = false;
+
+        } finally {
+            if (testStatus) {
+                testReport.pass(url);
+            } else {
+                testReport.fail("<b>URL: </b>" + url + "<br>" +
+                    "<b>Exception: </b>" + errorMessage);
+            }
+        }
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        extentReports.flush();
+    }
+
+    /**
+     * Create an HTTPConnection and get the responseCode. Based on that code the test will fail or not
+     * @param url the url provided on json file
+     * @return <i>true</i> if the endpoint is alive or <i>false</i> if not
+     * @throws Exception
+     */
+    private boolean healthCheckStatus(String url) throws Exception {
+        boolean status;
+        HttpURLConnection http;
+
+        if (Boolean.parseBoolean(ReadConfFile.returnValue("http.useProxy"))) {
+            setProxy();
+        }
+
+        http = (HttpURLConnection) new URL(url).openConnection();
+        http.setRequestMethod("GET");
+        http.setConnectTimeout((int)connectionTimeout);
+        http.connect();
+
+        if (!isServerResponding(http.getResponseCode())) {
+            status = false;
+        } else {
+            status = true;
+        }
+
+        System.out.println("name = " + name);
+        System.out.println("url = " + url);
+        System.out.println("-----");
+        return status;
+    }
+
+    private static long setConnectionTimeout() {
+        long connectionTimeoutParameter;
+
+        if(System.getProperty("timeout") == null) {
+
+            connectionTimeoutParameter = 10000;
+        } else {
+            connectionTimeoutParameter = TimeUnit.SECONDS.toMillis(Integer.parseInt(System.getProperty("timeout")));
+        }
+
+        return connectionTimeoutParameter;
+    }
+
+    private boolean isServerResponding(int statusCode) throws Exception {
+        int[] listOfStatusCode = {404, 408, 410, 451, 500, 501, 502, 503, 504, 505, 506, 507, 508, 510, 511};
+        boolean status = true;
+
+        for (int i = 0; i < listOfStatusCode.length; i++) {
+            if (statusCode == listOfStatusCode[i]) {
+                throw new Exception("HTTP Status: " + statusCode);
+            }
+        }
+
+        return status;
+    }
+
+    private static void setProxy() {
+        System.setProperty("http.proxyHost", ReadConfFile.returnValue("http.proxyHost"));
+        System.setProperty("http.proxyPort", ReadConfFile.returnValue("http.proxyPort"));
+        System.setProperty("https.proxyHost", ReadConfFile.returnValue("https.proxyHost"));
+        System.setProperty("https.proxyPort", ReadConfFile.returnValue("https.proxyPort"));
+    }
+}
+
